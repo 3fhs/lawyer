36d322cf2141ee0da23af9034e9063ebac5145c7
3fhs <abdelghany654@gmail.com>
2024-11-04 10:30:26 +0200

    1

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,32 +1,20 @@
+// Theme.js
 export const getDesignTokens = (mode) => ({
   palette: {
     mode,
-    ...(mode === "light"
-      ? {
-          // قيم الألوان في وضع الإضاءة
-          primary: {
-            main: '#1976d2',
-          },
-          secondary: {
-            main: '#ff4081',
-          },
-          background: {
-            default: '#ffffff',
-            paper: '#f5f5f5',
-          },
-        }
-      : {
-          // قيم الألوان في الوضع الداكن
-          primary: {
-            main: '#90caf9',
-          },
-          secondary: {
-            main: '#f48fb1',
-          },
-          background: {
-            default: '#121212',
-            paper: '#1d1d1d',
-          },
-        }),
+    primary: {
+      main: mode === 'light' ? '#1976d2' : '#90caf9', // الأزرق للضوء، الأزرق الفاتح للداكن
+    },
+    secondary: {
+      main: mode === 'light' ? '#dc004e' : '#f48fb1', // الأحمر للضوء، الوردي للداكن
+    },
+    background: {
+      default: mode === 'light' ? '#ffffff' : '#121212', // اللون الافتراضي للخلفية
+      paper: mode === 'light' ? '#f5f5f5' : '#424242', // اللون لخلفية الورق
+    },
+    text: {
+      primary: mode === 'light' ? '#000000' : '#ffffff', // اللون الافتراضي للنص
+      secondary: mode === 'light' ? '#7a7a7a' : '#b3b3b3',
+    },
   },
 });
